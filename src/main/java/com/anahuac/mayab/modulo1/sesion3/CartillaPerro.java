1c44c55fe0a35cbd4779b0b54da15b8c0ddcf654
Interian Eduardo <eduardointerian1000@gmail.com>
2024-11-15 19:06:06 -0600

    error

--- src/main/java/com/anahuac/mayab/modulo1/sesion3/CartillaPerro.java
+++ src/main/java/com/anahuac/mayab/modulo1/sesion3/CartillaPerro.java
@@ -48,7 +48,7 @@ public class CartillaPerro {
 			fechas.add(fechaAplicacion);
 			vacunas.put(nombreVacuna, fechas);
 		}
-		else { //NO existe
+		else {
 			ArrayList<String> fechas = new ArrayList<>();
 			fechas.add(fechaAplicacion);
 			vacunas.put(nombreVacuna, fechas);
